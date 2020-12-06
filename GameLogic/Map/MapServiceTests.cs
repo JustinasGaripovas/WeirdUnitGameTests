00c6e70b164644c7f38c509b43939dc6a828aa4d
Justin <Justin@DESKTOP-FM1CJC1>
2020-12-06 17:59:25 +0200

    feat: Added map service tests

--- GameLogic/Map/MapServiceTests.cs
+++ GameLogic/Map/MapServiceTests.cs
@@ -12,43 +12,182 @@ namespace WeirdUnitBE.GameLogic.Map.Tests
         [TestMethod()]
         public void GetInitialTowerPositionTest()
         {
-            Assert.Fail();
+            Position initialTowerPosition = MapService.GetInitialTowerPosition();
+
+            Assert.Equals(initialTowerPosition, new Position(0, 4));
         }
 
         [TestMethod()]
         public void GetInitialEnemyTowerPositionTest()
         {
-            Assert.Fail();
+            Position initialTowerPosition = MapService.GetInitialEnemyTowerPosition();
+
+            Assert.Equals(initialTowerPosition, new Position(9, 5));
         }
 
         [TestMethod()]
         public void GetDefaultMapConnectionsTest()
         {
-            Assert.Fail();
+            IDictionary<Position, Position[]> conenctionsBetweenTowers = MapService.GetDefaultMapConnections();
+            IDictionary<Position, Position[]> expectedConnections = new Dictionary<Position, Position[]>();
+
+            expectedConnections[new Position(0, 4)] =
+                new Position[]
+                {
+                    new Position(0, 2),
+                    new Position(2, 2),
+                    new Position(2, 6),
+                };
+
+            expectedConnections[new Position(0, 2)] =
+                new Position[]
+                {
+                    new Position(0, 4),
+                    new Position(2, 2),
+                };
+
+            expectedConnections[new Position(2, 2)] =
+                new Position[]
+                {
+                    new Position(0, 4),
+                    new Position(0, 2),
+                    new Position(4, 1),
+                    new Position(3, 4),
+                };
+
+            expectedConnections[new Position(4, 1)] =
+                new Position[]
+                {
+                    new Position(7, 3),
+                    new Position(2, 2),
+                    new Position(3, 4),
+                };
+
+            expectedConnections[new Position(3, 4)] =
+                new Position[]
+                {
+                    new Position(4, 1),
+                    new Position(2, 2),
+                    new Position(6, 5),
+                    new Position(2, 6),
+                };
+
+            expectedConnections[new Position(2, 6)] =
+                new Position[]
+                {
+                    new Position(3, 4),
+                    new Position(0, 4),
+                    new Position(5, 8),
+                };
+
+            expectedConnections[new Position(7, 3)] =
+                new Position[]
+                {
+                    new Position(4, 1),
+                    new Position(9, 5),
+                    new Position(6, 5),
+                };
+
+            expectedConnections[new Position(9, 5)] =
+                new Position[]
+                {
+                    new Position(9, 7),
+                    new Position(7, 7),
+                    new Position(7, 3),
+                };
+
+            expectedConnections[new Position(9, 7)] =
+                new Position[]
+                {
+                    new Position(9, 5),
+                    new Position(7, 7),
+                };
+
+            expectedConnections[new Position(7, 7)] =
+                new Position[]
+                {
+                    new Position(6, 5),
+                    new Position(9, 5),
+                    new Position(9, 7),
+                    new Position(5, 8),
+                };
+
+            expectedConnections[new Position(5, 8)] =
+                new Position[]
+                {
+                    new Position(2, 6),
+                    new Position(6, 5),
+                    new Position(7, 7),
+                };
+
+            expectedConnections[new Position(6, 5)] =
+                new Position[]
+                {
+                    new Position(7, 3),
+                    new Position(7, 7),
+                    new Position(5, 8),
+                    new Position(3, 4),
+                };
+
+            Assert.AreSame(conenctionsBetweenTowers, expectedConnections);
         }
 
         [TestMethod()]
         public void GetDefaultMapTest()
         {
-            Assert.Fail();
+            List<Position> expectedPositions = new List<Position>();
+
+            expectedPositions.Add(new Position(0, 4));
+            expectedPositions.Add(new Position(0, 2));
+            expectedPositions.Add(new Position(2, 2));
+            expectedPositions.Add(new Position(4, 1));
+            expectedPositions.Add(new Position(3, 4));
+            expectedPositions.Add(new Position(2, 6));
+            expectedPositions.Add(new Position(7, 3));
+            expectedPositions.Add(new Position(9, 5));
+            expectedPositions.Add(new Position(9, 7));
+            expectedPositions.Add(new Position(7, 7));
+            expectedPositions.Add(new Position(5, 8));
+            expectedPositions.Add(new Position(6, 5));
+
+            Assert.AreSame(expectedPositions, MapService.GetDefaultMap());
         }
 
         [TestMethod()]
         public void GetDefaultMapDimensionsTest()
         {
-            Assert.Fail();
+            Assert.AreSame((10, 10), MapService.GetDefaultMapDimensions());
         }
 
         [TestMethod()]
         public void GetDefaultGameSpeedTest()
         {
-            Assert.Fail();
+            Assert.AreSame(0.3f, MapService.GetDefaultGameSpeed());
         }
 
         [TestMethod()]
         public void GetDefaultMapWithoutInitialTowersTest()
         {
-            Assert.Fail();
+            List<Position> expectedPositions = new List<Position>();
+
+            expectedPositions.Add(new Position(0, 4));
+            expectedPositions.Add(new Position(0, 2));
+            expectedPositions.Add(new Position(2, 2));
+            expectedPositions.Add(new Position(4, 1));
+            expectedPositions.Add(new Position(3, 4));
+            expectedPositions.Add(new Position(2, 6));
+            expectedPositions.Add(new Position(7, 3));
+            expectedPositions.Add(new Position(9, 5));
+            expectedPositions.Add(new Position(9, 7));
+            expectedPositions.Add(new Position(7, 7));
+            expectedPositions.Add(new Position(5, 8));
+            expectedPositions.Add(new Position(6, 5));
+
+            expectedPositions.Remove(new Position(0, 4));
+            expectedPositions.Remove(new Position(9, 5));
+
+
+            Assert.AreSame(expectedPositions, MapService.GetDefaultMapWithoutInitialTowers());
         }
     }
 }
